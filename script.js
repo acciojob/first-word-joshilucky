c8190be275980b0744a0e1542567540d3ac4706e
Lucky Joshi <146523367+joshilucky@users.noreply.github.com>
2025-11-03 15:35:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+  // Trim to remove leading/trailing spaces
+  str = str.trim();
+
+  // If string is empty after trimming, return empty string
+  if (str === "") return "";
+
+  // Find index of first space
+  const spaceIndex = str.indexOf(" ");
+
+  // If no space found, return entire string
+  if (spaceIndex === -1) return str;
+
+  // Otherwise, return substring from start to first space
+  return str.substring(0, spaceIndex);
 }
-
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
